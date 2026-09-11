f05e355d946d1210d990468b212a9244f0ffcfe5
Jayesh Kute <jayeshkute03@gmail.com>
2026-09-11 15:39:30 +0530

    Update frontend

--- frontend/app.py
+++ frontend/app.py
@@ -9,7 +9,7 @@ import gradio as gr
 
 API_URL = os.getenv(
     "API_URL",
-    " https://student-management-api-hv43.onrender.com"
+    "https://student-management-api-hv43.onrender.com"
 ).rstrip("/")
 
 
@@ -503,6 +503,7 @@ if __name__ == "__main__":
     )
 
     demo.launch(
-        server_name="127.0.0.1",
+        server_name="0.0.0.0",
         server_port=port
     )
+
